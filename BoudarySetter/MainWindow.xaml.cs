a6245bfdc68021d8a241632bc07c813d93852c3d
tomoyukiOkano <pyonsun16-okano@ezweb.ne.jp>
2020-10-04 21:13:31 +0900

    new

--- BoudarySetter/MainWindow.xaml.cs
+++ BoudarySetter/MainWindow.xaml.cs
@@ -12,17 +12,23 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using OpenCvSharp;
 
 namespace BoudarySetter
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
